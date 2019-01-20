20fd98690f7f5e6c14801a8c5fe246da7f2d70d3
Czakero <bartosz.pyrz96@gmail.com>
2019-01-20 01:25:36 +0100

    Implement runnable listprinter class which handles printing data about watches

---
+++ src/main/java/com/codecool/ListPrinter.java
@@ -0,0 +1,21 @@
+package com.codecool;
+
+import java.util.List;
+
+public class ListPrinter implements Runnable {
+
+    private List<Watch> watchList;
+
+    public ListPrinter(List<Watch> watchList) {
+        this.watchList = watchList;
+    }
+    @Override
+    public void run() {
+        for (Watch watch : watchList) {
+            System.out.println("Thread id: " + watch.getThread_id());
+            System.out.println("Watch name: " + watch.getName());
+            System.out.println("Watch run-time: " + watch.getSeconds() + " seconds");
+            System.out.println("--------------------------------------------------");
+        }
+    }
+}
